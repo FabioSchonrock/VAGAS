82dc4136cc7b0213903b9717fce91e6b25604d6f
Fabio Schonrock <fabio.schonrock@gmail.com>
2022-02-08 20:19:53 -0300

    primeiro commit

---
+++ vagas/cadastrar.php
@@ -0,0 +1,20 @@
+<?php
+
+use app\Entity\Vaga;
+
+require __DIR__.'/vendor/autoload.php';
+
+//VALIDAÇÃO DO POST - SE ESSES CAMPOS ESTIVEREM PREENCHIDOS (SETADOS)
+ if(isset($_POST['titulo'],$_POST['descricao'],$_POST['ativo'])){
+      $obVaga = new Vaga;
+      $obVaga->titulo    = $_POST['titulo'];
+      $obVaga->descricao = $_POST['descricao'];
+      $obVaga->ativo     = $_POST['ativo'];
+        
+    echo "<pre>";print_r($_POST);"</pre>";
+                  
+}
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/formulario.php';
+include __DIR__.'/includes/footer.php';
---
+++ vagas/includes/header.php
@@ -0,0 +1,23 @@
+<!--HEADER VAI SER/CUIDAR O NOSSO CABEÇALHO-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>WDEV VAGAS</title>
+  </head>
+  <body class="bg-dark text-light"> <!--deixou o fundo escuro -->
+
+    <div class="container"> <!--CONTAINER -> DEIXA O NOSSO TEXTO CENTRALIZADO-->
+
+        <div class="jumbotron bg-danger">
+            <h1 class="text-center">WDEV vagas
+            <p class="text-center">Exemplo de CRUD com PHP orientados a objetos</p>
+            </h1>
+        </div>
